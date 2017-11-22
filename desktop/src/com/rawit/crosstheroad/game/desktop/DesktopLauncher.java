d6935fd562155b709903c820891cad8a4f3dc3c9
rawitP <rawit.p@ku.th>
2017-11-22 21:43:56 +0700

    Initialize git

---
+++ desktop/src/com/rawit/crosstheroad/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rawit.crosstheroad.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rawit.crosstheroad.game.CrossTheRoadGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrossTheRoadGame(), config);
+	}
+}
