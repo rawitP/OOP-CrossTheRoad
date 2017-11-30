a498bfce2a97b977a2edc18f08bd0e6a6628f1f5
rawitP <rawit.p@ku.th>
2017-11-30 22:20:52 +0700

    Using OrthographicCamera for viewing.

--- core/src/com/rawit/crosstheroad/game/World.java
+++ core/src/com/rawit/crosstheroad/game/World.java
@@ -2,9 +2,18 @@ package com.rawit.crosstheroad.game;
 
 public class World {
 
+    private static final int CAM_VIEWPORT_WIDTH = 540;
+    private static final int CAM_VIEWPORT_HEIGHT = 960;
+
+    private static final int WORLD_WIDTH = CAM_VIEWPORT_WIDTH;
+    private static final int WORLD_HEIGHT = 2 * CAM_VIEWPORT_HEIGHT;
+
+    private static final float CAM_INIT_SPEED = 1;
+
     private CrossTheRoadGame crossTheRoadGame;
     private Map map;
     private Chicken chicken;
+    public Camera cam;
 
     World(CrossTheRoadGame crossTheRoadGame) {
         this.crossTheRoadGame = crossTheRoadGame;
@@ -12,8 +21,12 @@ public class World {
     }
 
     private void initWorld() {
-        map = new Map(CrossTheRoadGame.WIDTH, CrossTheRoadGame.HEIGHT, this);
-        map.setSpeed(1);
+        cam = new Camera(CAM_VIEWPORT_WIDTH, CAM_VIEWPORT_HEIGHT);
+        cam.setSpeed(CAM_INIT_SPEED);
+        cam.x = WORLD_WIDTH / 2.0f;
+        cam.y = CAM_VIEWPORT_HEIGHT / 2.0f + 0;
+
+        map = new Map(WORLD_WIDTH, WORLD_HEIGHT, this);
         chicken = new Chicken(0, 0, this);
     }
 
@@ -26,6 +39,7 @@ public class World {
     }
 
     public void update(float delta) {
+        cam.update(delta);
         map.update(delta);
         chicken.update(delta);
     }
